e599a4e049f08e2c855f862049f95bc08cc81235
ruddyoke <lruddy33@gmail.com>
2017-01-10 14:50:27 +0100

    Update date.js

--- date.js
+++ date.js
@@ -1,27 +1,26 @@
 if ( !Date.prototype.toISOString2BWA ) {
-  ( function() {
-    
-    function pad(number) {
-      if ( number < 10 ) {
-        return '0' + number;
-      }
-      return number;
-    }
- 
-    Date.prototype.toISOString2 = function(delimiter) {
-        delimiter = typeof delimiter !== 'undefined' ? delimiter : '-';
-        return this.getUTCFullYear() +
-            delimiter + pad( this.getUTCMonth() + 1 ) +
-            delimiter + pad( this.getUTCDate() );
-    };
+    ( function() {
 
-    Date.prototype.toISOString2Fr = function(delimiter) {
-        delimiter = typeof delimiter !== 'undefined' ? delimiter : '-';
-        return ''+ pad( this.getUTCDate() +
-            delimiter + pad( this.getUTCMonth() + 1 ) +
-            delimiter +  this.getUTCFullYear() )
-            ;
-    };
-  
-  }() );
-}
+        function pad(number) {
+            if ( number < 10 ) {
+                return '0' + number;
+            }
+            return number;
+        }
+
+        Date.prototype.toISOStringYMD = function(delimiter) {
+            delimiter = typeof delimiter !== 'undefined' ? delimiter : '-';
+            return this.getUTCFullYear() +
+                delimiter + pad( this.getUTCMonth() + 1 ) +
+                delimiter + pad( this.getUTCDate() );
+        };
+
+        Date.prototype.toISOStringDMY = function(delimiter) {
+            delimiter = typeof delimiter !== 'undefined' ? delimiter : '-';
+            return ''+ pad( this.getUTCDate() +
+                    delimiter + pad( this.getUTCMonth() + 1 ) +
+                    delimiter +  this.getUTCFullYear() )
+                ;
+        };
+
+    }() );
